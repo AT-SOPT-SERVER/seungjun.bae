3fb4a7ecc701d68afe86296b3a527bd19b885964
lezgititt <155799025+lezgititt@users.noreply.github.com>
2025-04-13 03:56:33 +0900

    코드보완 최종

--- src/main/java/org/sopt/validation/PostValidator.java
+++ src/main/java/org/sopt/validation/PostValidator.java
@@ -17,7 +17,7 @@ public class PostValidator {
     }
 
     public static void postTime(LocalDateTime latestPostTime){
-        if(latestPostTime==null || Duration.between(latestPostTime , LocalDateTime.now()).getSeconds()>=1) {
+        if(latestPostTime==null || Duration.between(latestPostTime , LocalDateTime.now()).toMinutes()>=3) {
             latestPostTime = LocalDateTime.now();
         } else {
             throw new IllegalArgumentException((180-(int)Duration.between(latestPostTime , LocalDateTime.now()).getSeconds()) + "초 뒤에 게시물을 다시 작성할 수 있어요.");
