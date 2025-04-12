2f35f4b7986f3ab4b9320335d7b28080dea3ef9d
lezgititt <155799025+lezgititt@users.noreply.github.com>
2025-04-13 03:38:32 +0900

    코드리뷰 후 코드보완. 더 객체지향적으로

--- src/main/java/org/sopt/controller/PostController.java
+++ src/main/java/org/sopt/controller/PostController.java
@@ -2,6 +2,7 @@ package org.sopt.controller;
 
 import org.sopt.domain.Post;
 import org.sopt.service.PostService;
+import org.sopt.validation.PostValidator;
 
 import java.util.List;
 
@@ -9,7 +10,13 @@ public class PostController {
     private final PostService postService = new PostService();
 
     public void createPost(String title){
-        postService.createPost(title);
+        //제목 글자수 검사
+        try {
+            PostValidator.titleLength(title);
+            postService.createPost(title);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
     }
 
     public List<Post> getAllPosts(){
@@ -25,7 +32,14 @@ public class PostController {
     }
 
     public boolean updatePostTitle(int id, String newTitle){
-        return postService.updateTitleById(id, newTitle);
+        //제목 글자수 검사
+        try {
+            PostValidator.titleLength(newTitle);
+            return postService.updateTitleById(id, newTitle);
+        } catch (IllegalArgumentException e){
+            System.out.println(e.getMessage());
+            return false;
+        }
     }
 
     public List<Post> searchPostsByKeyword(String keyword){
---
+++ src/main/java/org/sopt/validation/PostValidator.java
@@ -0,0 +1,27 @@
+package org.sopt.validation;
+
+import org.sopt.domain.Post;
+
+import java.time.Duration;
+import java.time.LocalDateTime;
+
+public class PostValidator {
+
+    public static void titleLength(String title){
+        if (title.isEmpty()) {
+            throw new IllegalArgumentException("제목을 입력해주세요");
+        }
+        if (title.length()>30){
+            throw new IllegalArgumentException("제목은 최대 30자까지 작성할 수 있어요");
+        }
+    }
+
+    public static void postTime(LocalDateTime latestPostTime){
+        if(latestPostTime==null || Duration.between(latestPostTime , LocalDateTime.now()).getSeconds()>=1) {
+            latestPostTime = LocalDateTime.now();
+        } else {
+            throw new IllegalArgumentException((180-(int)Duration.between(latestPostTime , LocalDateTime.now()).getSeconds()) + "초 뒤에 게시물을 다시 작성할 수 있어요.");
+        }
+    }
+
+}
